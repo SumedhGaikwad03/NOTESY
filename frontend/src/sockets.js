47883291cd86731256d8f0638d7c515b63514cb5
realsaitama <777sumedh777@gmail.com>
2026-02-24 18:51:53 +0530

    socket -updated2

--- frontend/src/sockets.js
+++ frontend/src/sockets.js
@@ -1,12 +1,16 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
-  autoConnect: false
+const SOCKET_URL =
+  process.env.REACT_APP_API_URL?.replace("/api", "") ||
+  "http://localhost:5000";
+
+const socket = io(SOCKET_URL, {
+  autoConnect: false,
 });
 
 export const connectSocket = () => {
   socket.auth = {
-    token: localStorage.getItem("token")
+    token: localStorage.getItem("token"),
   };
   socket.connect();
 };
