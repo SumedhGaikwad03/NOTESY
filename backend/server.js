df19bd74c23278916b1bb03679fa0ec9479c976a
realsaitama <777sumedh777@gmail.com>
2026-03-15 22:53:53 +0530

    domain change

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,7 @@ const app = express();
 app.use(cors({
   origin: [
     "https://notesy-sumedh-gaikwads-projects.vercel.app",
+    "https://atrio-gamma.vercel.app/",
     "http://localhost:3000",
     "http://localhost:5173"
   ],
